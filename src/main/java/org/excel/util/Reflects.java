5de6d1a44158466e1dc040298109f0e4f7a03799
zzh <zzh@wooduan.com>
2019-04-18 16:01:35 +0800

    add Reflect util

---
+++ src/main/java/org/excel/util/Reflects.java
@@ -0,0 +1,60 @@
+package org.excel.util;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+
+public final class Reflects {
+
+    private Reflects() {
+        //  no-op
+    }
+
+    /**
+     * Create new instance with clazz.
+     */
+    public static <T> T newInstance(Class<T> clazz, Object... params) {
+        Constructor<T> constructor = lookupConstructor(clazz, params);
+        if (null == constructor)
+            return null;
+        try {
+            if (params.length <= 0) {
+                return constructor.newInstance();
+            } else {
+                return constructor.newInstance(params);
+            }
+        } catch (Exception e) {
+            //  no-op
+        }
+        return null;
+    }
+
+    public static <T> Constructor<T> lookupConstructor(Class<T> clazz, Object... params) {
+        Constructor<T> constructor = null;
+        if (params.length <= 0) {
+            try {
+                constructor = clazz.getConstructor();
+            } catch (NoSuchMethodException e) {
+                try {
+                    constructor = clazz.getDeclaredConstructor();
+                } catch (NoSuchMethodException e1) {
+                    //  no-op
+                }
+            }
+        } else {
+            Class[] classes = Arrays.stream(params).map(Object::getClass).toArray(Class[]::new);
+            try {
+                constructor = clazz.getConstructor(classes);
+            } catch (NoSuchMethodException e) {
+                try {
+                    constructor = clazz.getDeclaredConstructor(classes);
+                } catch (NoSuchMethodException e1) {
+                    //  no-op
+                }
+            }
+        }
+        if (constructor != null) {
+            constructor.setAccessible(true);
+        }
+        return constructor;
+    }
+}
