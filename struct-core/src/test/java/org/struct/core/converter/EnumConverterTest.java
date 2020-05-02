435a57f0f9fbca8e9918cd471cfee0c6922a04b4
TinyZ <tinyzzh815@gmail.com>
2020-05-03 03:10:06 +0800

    完善测试用例EnumConverter

--- struct-core/src/test/java/org/struct/core/converter/EnumConverterTest.java
+++ struct-core/src/test/java/org/struct/core/converter/EnumConverterTest.java
@@ -34,12 +34,19 @@ public class EnumConverterTest {
         Assert.assertEquals(MyEnum.Two, converter.convert("1", MyEnum.class));
         Assert.assertEquals(MyEnum.Three, converter.convert("2", MyEnum.class));
 
+        Assert.assertEquals(MyEnum.One, converter.convert("One", MyEnum.class));
+        Assert.assertEquals(MyEnum.Two, converter.convert("Two", MyEnum.class));
+        Assert.assertEquals(MyEnum.Three, converter.convert("Three", MyEnum.class));
+
         Assert.assertEquals(MyEnum.One, converter.convert("one", MyEnum.class));
         Assert.assertEquals(MyEnum.Two, converter.convert("two", MyEnum.class));
         Assert.assertEquals(MyEnum.Three, converter.convert("three", MyEnum.class));
 
         Assert.assertEquals("three", converter.convert("three", Integer.class));
 
+        Assert.assertEquals(MyEnum.One, converter.convert(MyEnum.One, MyEnum.class));
+        Assert.assertEquals(MyEnum.Two, converter.convert(MyEnum.Two, MyEnum.class));
+        Assert.assertEquals(MyEnum.Three, converter.convert(MyEnum.Three, MyEnum.class));
         try {
             converter.convert("four", MyEnum.class);
         } catch (IllegalStateException e) {
