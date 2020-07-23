1099db27c8a59161463984f1cb47dad5269534fa
TinyZ <tinyzzh815@gmail.com>
2020-07-23 16:17:26 +0800

    update Copyright

--- struct-spring-boot-starter/src/main/java/org/struct/spring/boot/autoconfigure/StructAutoConfiguration.java
+++ struct-spring-boot-starter/src/main/java/org/struct/spring/boot/autoconfigure/StructAutoConfiguration.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *
+ *          Copyright (c) 2020. - TinyZ.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.struct.spring.boot.autoconfigure;
 
 import org.slf4j.Logger;
@@ -24,8 +42,9 @@ import org.springframework.util.StringUtils;
 import org.struct.annotation.StructSheet;
 import org.struct.spring.support.ClassPathStructScanner;
 import org.struct.spring.support.StructConfig;
-import org.struct.spring.support.StructStoreService;
+import org.struct.spring.support.StructScannerRegistrar;
 import org.struct.spring.support.StructStore;
+import org.struct.spring.support.StructStoreService;
 import org.struct.support.FileWatcherService;
 import org.struct.util.WorkerUtil;
 
@@ -117,7 +136,7 @@ public class StructAutoConfiguration {
 
         @Override
         public void afterPropertiesSet() {
-            LOGGER.debug("Not found configuration for StructScan.");
+            LOGGER.debug("Not found configuration for {}.", StructStoreService.class.getName());
         }
     }
 
