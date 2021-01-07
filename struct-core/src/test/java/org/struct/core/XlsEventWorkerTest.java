71385c4df85073490e62281f3e51c5b92754650f
TinyZ <tinyzzh815@gmail.com>
2021-01-07 15:36:40 +0800

    Update Copyright :mouse:`2020` -> :cow:`2021`

--- struct-core/src/test/java/org/struct/core/XlsEventWorkerTest.java
+++ struct-core/src/test/java/org/struct/core/XlsEventWorkerTest.java
@@ -1,7 +1,7 @@
 /*
  *
  *
- *          Copyright (c) 2019. - TinyZ.
+ *          Copyright (c) 2021. - TinyZ.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
